da4d703eae12ea318afdbda046068657fbb467b3
Joachim Schipper <j.schipper@math.uu.nl>
2010-07-11 15:08:17 +0200

    Output delimiter defaults to \n
    This plays better with other tools.
    
    While there, update the copyright line (long overdue).

--- randomize.c
+++ randomize.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 Joachim Schipper <joachim@joachimschipper.nl>
+ * Copyright (c) 2009, 2010 Joachim Schipper <joachim@joachimschipper.nl>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -121,7 +121,7 @@ main(int argc, char **argv)
 
 	/* Defaults */
 	re_str = "\n";
-	output_str = "&";
+	output_str = "\n";
 	nrecords = UINT32_MAX;
 
 	while ((ch = getopt(argc, argv, "e:n:o:")) != -1) {

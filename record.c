d84c0a564b6fc70ce04075f6455167873655c38c
Joachim Schipper <joachim@joachimschipper.nl>
2011-03-24 18:50:13 +0100

    Fix bug exposed by pcre-8.10+
    From the ChangeLog file, under 8.10:
    
    14. Neither pcre_exec() nor pcre_dfa_exec() was checking that the value given
        as a starting offset was within the subject string. There is now a new
        error, PCRE_ERROR_BADOFFSET, which is returned if the starting offset is
        negative or greater than the length of the string. In order to test this,
        pcretest is extended to allow the setting of negative starting offsets.
    
    Change the call to pcre_exec() inside rec_next(): we previously assumed
    that pcre_exec would use subject[startoffset]..buf[startoffset + length];
    change to subject[startoffset]..buf[length].
    
    This makes randomize work with pcre-8.10+.

--- record.c
+++ record.c
@@ -323,7 +323,7 @@ rec_next(int rfd, struct rec *rec)
 	 * this code.
 	 */
 	eof = 0;
-	while ((rv = pcre_exec(f[rfd].re, f[rfd].re_extra, f[rfd].buf_p, f[rfd].buf_last - f[rfd].buf_first_read, f[rfd].buf_first_read, eof ? 0 : PCRE_NOTEOL, ovector, ovector_size)) < 0) {
+	while ((rv = pcre_exec(f[rfd].re, f[rfd].re_extra, f[rfd].buf_p, f[rfd].buf_last, f[rfd].buf_first_read, eof ? 0 : PCRE_NOTEOL, ovector, ovector_size)) < 0) {
 		if (rv != PCRE_ERROR_NOMATCH) {
 			errno = EINVAL;
 			goto err;
